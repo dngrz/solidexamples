15a6c75b569e6c07657b8f86752518861d9a8c48
Clodoaldo Sanchez <clodoaldosanchez@gmail.com>
2023-01-26 10:30:04 -0500

    base line

---
+++ src/main/java/com/cloudexperts/tech/solidexamples/SolidExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.cloudexperts.tech.solidexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudexperts/tech/solidexamples/SolidExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudexperts.tech.solidexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
